071b4dd7c6820d8901ade43d21f6f5051b131371
Andrey Kozitsyn <balin87@mail.ru>
2021-04-17 14:00:34 +0500

    Добавил тест на deleteIssue

--- 3-Unit-Testing/bad-architecture/tests/fetch-issues.test.js
+++ 3-Unit-Testing/bad-architecture/tests/fetch-issues.test.js
@@ -1,31 +1,31 @@
 //import { beforeAll, jest, expect } from '@jest/globals';
-import { fetchIssues } from '../src/main';
+import { fetchIssues, deleteIssue } from '../src/main';
 
 describe('fetchIssues function:', () => {
+  const ID_1 = "060e3461-baa1-5c97-86f3-8983f85823f2";
+  const ID_2 = "cb7de04c-7744-50e4-8a59-917efcd62196";
+  const fakeData = [
+    {
+      id: ID_1,
+      description: "sdfsdf",
+      severity: "Medium",
+      assignedTo: "23r",
+      status: "Open"
+    },
+    {
+      id: ID_2,
+      description: "sdfa",
+      severity: "Medium",
+      assignedTo: "3333",
+      status: "Open"
+    }
+  ];
+
   it('DOM renders correctly for non empty data', () => {
 
     expect.assertions(2);
 
     //1. "Загрузить" данные
-
-    const ID_1 = "060e3461-baa1-5c97-86f3-8983f85823f2";
-    const ID_2 = "cb7de04c-7744-50e4-8a59-917efcd62196";
-    const fakeData = [
-      {
-        id: ID_1,
-        description: "sdfsdf",
-        severity: "Medium",
-        assignedTo: "23r",
-        status: "Open"
-      },
-      {
-        id: ID_2,
-        description: "sdfa",
-        severity: "Medium",
-        assignedTo: "3333",
-        status: "Open"
-      }
-    ];
 
     JSON.parse = jest.fn().mockReturnValue(fakeData);
 
@@ -78,4 +78,16 @@ describe('fetchIssues function:', () => {
       expect(e.message).toEqual(errorMsg);
     }
   });
+
+  it('test DeleteIssue', () => {
+    JSON.parse = jest.fn().mockReturnValue(fakeData);
+    JSON.stringify = jest.fn();
+    deleteIssue(ID_2);
+    const settedItems = JSON.stringify.mock.calls[0][0];
+
+    // Проверяем что среди устанавливаемых в localStorage элементов нет удаляемого
+    expect(settedItems.length).toEqual(1);
+    expect(settedItems).toContain(fakeData[0]);
+    expect(settedItems).not.toContain(fakeData[1]);
+  })
 })
